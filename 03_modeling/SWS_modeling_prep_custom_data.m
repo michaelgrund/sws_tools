7286411d1def2d4a1824960c340c14513e3e13d1
Michael Grund <michael.grund@kit.edu>
2020-06-16 12:40:20 +0200

    added new file

---
+++ 03_modeling/SWS_modeling_prep_custom_data.m
@@ -0,0 +1,16 @@
+function SWS_modeling_prep_custom_data()
+%
+% template script to prepare your own splitting data for the modeling
+% routines
+%
+% you can use this script especially if your splitting results are not 
+% available in SplitLab/StackSplit output format 
+
+BAZ
+phiSC + errors
+dtSC + errors
+
+single_splits
+nulls
+stacked_split
+
